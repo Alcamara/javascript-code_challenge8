d3f19f7af3ec6dc4680c40fa6d291d576a31cb6f
alcamara <alhousseinec@gmail.com>
2024-09-10 20:45:04 -0500

    added inputUser func and vendingProducts array

--- problem8.js
+++ problem8.js
@@ -1,22 +1,37 @@
-/**
- * 
- * Problem:
+// Problem:
 
- You are tasked with writing a program that simulates a simple vending machine. The vending machine offers the following products:
+//  You are tasked with writing a program that simulates a simple vending machine. The vending machine offers the following products:
 
-    1: Soda - $1.25
-    2: Chips - $1.50
-    3: Candy - $0.75
+//     1: Soda - $1.25
+//     2: Chips - $1.50
+//     3: Candy - $0.75
 
- Write a function called vendingMachine that:
+//  Write a function called vendingMachine that:
 
-    Takes an array of product numbers as input.
-    Uses a loop to process each product in the array.
-    Uses a switch statement to determine the price of each product.
-    Keeps track of the total amount the user needs to pay.
-    Prints each product name and price as it's processed.
-    After processing all products, prints the total price.
+//     Takes an array of product numbers as input.
+//     Uses a loop to process each product in the array.
+//     Uses a switch statement to determine the price of each product.
+//     Keeps track of the total amount the user needs to pay.
+//     Prints each product name and price as it's processed.
+//     After processing all products, prints the total price.
 
- If an invalid product number is encountered (i.e., not 1, 2, or 3), it should print a message saying "Invalid product."
- * 
- * /
+//  If an invalid product number is encountered (i.e., not 1, 2, or 3), it should print a message saying "Invalid product."
+
+const prompt = require('prompt')
+
+ const vendingProducts = [
+    {
+        name: "soda",
+        price: 1.25
+    },
+    {
+        name: "Chips",
+        price: 1.75
+    }, 
+    {
+        name: "candy",
+        price: .75
+    }]
+
+
+
