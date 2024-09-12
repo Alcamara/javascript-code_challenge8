ffaefe57498d1c25a62151f68a89f2b92431f4ec
alcamara <alhousseinec@gmail.com>
2024-09-11 21:30:21 -0500

    update vendingMachine func

--- problem8.js
+++ problem8.js
@@ -17,7 +17,6 @@
 
 //  If an invalid product number is encountered (i.e., not 1, 2, or 3), it should print a message saying "Invalid product."
 const prompt = require('prompt')
-
 const ProgramStatus = Object.freeze({
     Running : 0,
     Shutdown : 1,
@@ -27,22 +26,49 @@ const ProgramStatus = Object.freeze({
 
 const SelectStatus = Object.freeze({
     Done: 1,
-    Continue: 0
+    Continue: 0,
+    Cancel: 3
 })
 
 async function userInput(inputType){
     try {
-
+      let input = 0;
+      console.log(inputType);
+      
       switch (inputType) {
         case "programStatus":
-            return await prompt.get(inputType) 
+            const { programStatus } = await prompt.get("programStatus")
+            console.clear()
+            input = +programStatus
+            break; 
         case "itemNum":
-            return await prompt.get(inputType) 
+            const { itemNum } = await prompt.get("itemNum")
+            input = +itemNum
+            break; 
         case "purchaseStatus":
-            return await prompt.get(inputType) 
+            const { purchaseStatus } = await prompt.get("purchaseStatus")
+            console.clear()
+            input = +purchaseStatus
+            break;
+        case "confirm":
+        const { confirm } = await prompt.get("confirm")
+        console.clear()
+        input = +confirm
+        break;
+        case "again":
+            const { again } = await prompt.get("again")
+            input = +again
+            break;
         default:
             break;
-      }
+    }
+
+    if (isNaN(input)) {
+       console.log(msgPrompts[3])
+       return await userInput('again')
+    }
+
+    return input
 
     }catch(err){
         console.log(`${err}`)
@@ -73,70 +99,72 @@ const msgPrompts = [
     "Press 0 for Yes\nPress 1 for NO",
     "Invalid product!",
     "Thank you for stopping by, have a good day",
-    "Please choose item Below",
+    "Please choose item Below:",
     "Would you like to select another Items?",
-    "Great, let's get your total!" 
+    "Great, let's checkout!",
+    "Order has been canceled",
+    "The following Item(s) are in your cart:",
+    "Confirm Purchase?" 
 ]
 
 async function runVendingMachine(){
     let programRun = 0
-    const selectedProducts = []
+    let selectedProducts = []
     let result = 0
 
     while(programRun === ProgramStatus.Running){
         console.log(msgPrompts[0] +" "+msgPrompts[1]);
         console.log(msgPrompts[2]);
-        result = await userInput('programStatus')
+        const programStatus = await userInput('programStatus')
 
-        if (isNaN(+result.programStatus)) {
-            console.log(msgPrompts[3])
-            continue
-        }
-
-        if(+result.programStatus === ProgramStatus.Shutdown) {
+        if(programStatus === ProgramStatus.Shutdown) {
             console.log(msgPrompts[4])
             break
         }
 
-        let pickstatus = 0
-        while(pickstatus === SelectStatus.Continue) {
+        let SelectionStatus = 0
+        while(SelectionStatus === SelectStatus.Continue) {
+            console.log(msgPrompts[5]);
             for (const product of vendingProducts) {
                 console.log(`Press ${product.idItem} ${product.name} - ${product.price}`);
             }
 
-            const item = await userInput('itemNum')
+            const idItem = await userInput('itemNum')
+            const item = vendingProducts.find((product) => product.idItem === idItem)
+            console.clear()
+            console.log(`Add ${item.name} to cart.`)
+            selectedProducts.push(item)
 
-            if (isNaN(+item.itemNum)) {
-                console.log(msgPrompts[3])
-                continue
-            }
-
-            selectedProducts.push(+item.itemNum)
-
-            console.log(msgPrompts[6] + "\nPress 0 to Continue\nPress 1 to Be Done");
-            const s  = await userInput('purchaseStatus')
-
-            if (isNaN(+s.purchaseStatus)) {
-                console.log(msgPrompts[3])
-                continue
-            }
-
-            if(+s.purchaseStatus === SelectStatus.Done) {
+            console.log(msgPrompts[6] + "\nPress 0 to Continue\nPress 1 to Be Done\nPress 3 to Cancel");
+            const ss  = await userInput('purchaseStatus')
+            if(ss === SelectStatus.Done) {
                 console.log(msgPrompts[7])
+                await vendingMachine(selectedProducts)
                 break
+            } else if (ss === SelectStatus.Cancel){
+                selectedProducts = []
+                console.log(msgPrompts[8])
+                SelectionStatus++
+                break
+            } else if (ss === SelectStatus.Continue) {
+                continue
             }
 
         }
 
-        console.log(selectedProducts);
-        
-        
     }
 }
 
 
-function vendingMachine(){
-
+async function vendingMachine(itemsInCart){
+    let totalCost = 0
+    console.log(msgPrompts[9])
+    itemsInCart.forEach((item) => {
+        console.log(`${item.idItem} ${item.name} - ${item.price}`);
+        totalCost += item.price
+    })
+    console.log(`Your total today is ${totalCost}\n${msgPrompts[10]}\n0 yes\n1 no`)
+    const confirm = userInput('confirm')
 }
 
 runVendingMachine()
